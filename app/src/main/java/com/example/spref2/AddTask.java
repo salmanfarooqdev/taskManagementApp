b1049700ac357dfdf8c90eba34f0699e5372f4f8
salmanfarooqdev <slmnfrq94@gmail.com>
2024-04-27 17:08:28 +0500

    added add a task

--- app/src/main/java/com/example/spref2/AddTask.java
+++ app/src/main/java/com/example/spref2/AddTask.java
@@ -1,7 +1,16 @@
 package com.example.spref2;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -10,8 +19,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
+
 public class AddTask extends AppCompatActivity {
 
+
+    EditText dueDate;
+    ImageButton dueDateBtn;
+    Spinner dropdown;
+    Button addTaskBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +37,51 @@ public class AddTask extends AppCompatActivity {
         setContentView(R.layout.activity_add_task);
 
         init();
+
+        dueDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddTask.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Handle the selected date
+                        dueDate.setText(year + "-"+ (month+1) + "-"+ dayOfMonth);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        addTaskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sel = dropdown.getSelectedItem().toString();
+                Toast.makeText(AddTask.this, sel, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void init()
     {
         getSupportActionBar().setTitle("Add Task");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        dueDate = findViewById(R.id.etDate);
+        dueDateBtn = findViewById(R.id.etDateBtn);
+        dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"1", "2", "3", "4"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        addTaskBtn = findViewById(R.id.addTaskBtn);
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/spref2/Task.java
@@ -0,0 +1,64 @@
+package com.example.spref2;
+
+import java.util.Date;
+
+public class Task {
+
+    String taskTitle;
+    String taskDesc;
+    Date dueDate;
+    int priority;
+    boolean isCompleted;
+
+    public Task() {
+
+    }
+
+    public Task(String taskTitle, String taskDesc, Date dueDate, int priority, boolean isCompleted) {
+        this.taskTitle = taskTitle;
+        this.taskDesc = taskDesc;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.isCompleted = isCompleted;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskDesc() {
+        return taskDesc;
+    }
+
+    public void setTaskDesc(String taskDesc) {
+        this.taskDesc = taskDesc;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
